e914be4468c1f72dd5bbc54a817d82144a73c49c
Ryan Staudacher <RyanMStaudacher@gmail.com>
2018-05-06 05:03:09 -0500

    Added Player movement, Art Assets, Animations, etc.

---
+++ ECS_Final/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState { Idle, Walking, Sprinting, CrouchedIdle, CrouchedWalking };
+
+public class Player : MonoBehaviour
+{
+    public PlayerState playerState;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerState = PlayerState.Idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayerState();
+    }
+
+    //Detects input from player and switches player state accordingly
+    void UpdatePlayerState()
+    {
+        if (playerState == PlayerState.Idle)
+        {
+            if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0 ||
+                Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+            {
+                playerState = PlayerState.Walking;
+            }
+            if (Input.GetAxis("Vertical") > 0 && Input.GetButtonDown("Sprint"))
+            {
+                playerState = PlayerState.Sprinting;
+            }
+            if(Input.GetButtonDown("Crouch"))
+            {
+                playerState = PlayerState.CrouchedIdle;
+            }
+        }
+        else if (playerState == PlayerState.Walking)
+        {
+            if (Input.GetAxis("Vertical") > 0 && Input.GetButtonDown("Sprint"))
+            {
+                playerState = PlayerState.Sprinting;
+            }
+            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+                playerState = PlayerState.Idle;
+            }
+            if(Input.GetButtonDown("Crouch"))
+            {
+                playerState = PlayerState.CrouchedWalking;
+            }
+        }
+        else if (playerState == PlayerState.Sprinting)
+        {
+            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+                playerState = PlayerState.Idle;
+            }
+            if(!Input.GetButton("Sprint") || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+            {
+                playerState = PlayerState.Walking;
+            }
+            if(Input.GetButtonDown("Crouch"))
+            {
+                playerState = PlayerState.CrouchedWalking;
+            }
+        }
+        else if(playerState == PlayerState.CrouchedIdle)
+        {
+            if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0 ||
+                Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+            {
+                playerState = PlayerState.CrouchedWalking;
+            }
+            if(Input.GetButtonDown("Crouch"))
+            {
+                playerState = PlayerState.Idle;
+            }
+            if(Input.GetAxis("Vertical") > 0 && Input.GetButtonDown("Sprint"))
+            {
+                playerState = PlayerState.Sprinting;
+            }
+        }
+        else if(playerState == PlayerState.CrouchedWalking)
+        {
+            if(Input.GetButtonDown("Crouch"))
+            {
+                playerState = PlayerState.Walking;
+            }
+            if (Input.GetAxis("Vertical") > 0 && Input.GetButtonDown("Sprint"))
+            {
+                playerState = PlayerState.Sprinting;
+            }
+            if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            {
+                playerState = PlayerState.CrouchedIdle;
+            }
+        }
+    }
+}
---
+++ ECS_Final/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float mouseRotationSpeedX;
+    [SerializeField] private float mouseRotationSpeedY;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float crouchedWalkSpeed;
+
+    private Player playerScript;
+    private CharacterController playerController;
+    private float verticalRotation;
+    private float currentMoveSpeed;
+    private bool hasSetWalkSpeed = false;
+    private bool hasSetSprintSpeed = false;
+    private bool isCrouched = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        playerController = GetComponent<CharacterController>();
+        playerScript = GetComponent<Player>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayerLookAround();
+        ControlPlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void PlayerLookAround()
+    {
+        //Look right and left
+        if(Input.GetAxis("MouseX") > 0 || Input.GetAxis("MouseX") < 0)
+        {
+            this.transform.Rotate(0, mouseRotationSpeedY * Input.GetAxis("MouseX") * Time.deltaTime, 0);
+        }
+        //Look up and down
+        if(Input.GetAxis("MouseY") > 0 || Input.GetAxis("MouseY") < 0)
+        {
+            verticalRotation += Input.GetAxis("MouseY") * mouseRotationSpeedX * Time.deltaTime;
+
+            verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+            playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        }
+    }
+    
+    public void MovePlayer()
+    {
+        Vector3 playerMoveLeftRight = transform.right * Input.GetAxis("Horizontal") * currentMoveSpeed * Time.deltaTime;
+        Vector3 playerMoveForwardBackward = transform.forward * Input.GetAxis("Vertical") * currentMoveSpeed * Time.deltaTime;
+
+        playerController.SimpleMove(playerMoveLeftRight);
+        playerController.SimpleMove(playerMoveForwardBackward);
+    }
+
+    void ControlPlayer()
+    {
+        //Walk
+        if (playerScript.playerState == PlayerState.Walking)
+        {
+            if(hasSetWalkSpeed == false)
+            {
+                currentMoveSpeed = walkSpeed;
+                hasSetWalkSpeed = true;
+            }
+        }
+        else if(playerScript.playerState != PlayerState.Walking && hasSetWalkSpeed == true)
+        {
+            hasSetWalkSpeed = false;
+        }
+
+        //Sprint
+        if (playerScript.playerState == PlayerState.Sprinting)
+        {
+            if (hasSetSprintSpeed == false)
+            {
+                currentMoveSpeed = sprintSpeed;
+                hasSetSprintSpeed = true;
+            }
+        }
+        else if (playerScript.playerState != PlayerState.Sprinting && hasSetSprintSpeed == true)
+        {
+            hasSetSprintSpeed = false;
+        }
+
+        //Crouch
+        if (playerScript.playerState == PlayerState.CrouchedIdle || playerScript.playerState == PlayerState.CrouchedWalking)
+        {
+            if (isCrouched == false)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 0.5f, transform.localScale.z);
+                currentMoveSpeed = crouchedWalkSpeed;
+                isCrouched = true;
+            }
+        }
+
+        //Stand
+        if (playerScript.playerState != PlayerState.CrouchedIdle && playerScript.playerState != PlayerState.CrouchedWalking)
+        {
+            if (isCrouched == true)
+            {
+                transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 2f, transform.localScale.z);
+                isCrouched = false;
+            }
+        }
+    }
+}
